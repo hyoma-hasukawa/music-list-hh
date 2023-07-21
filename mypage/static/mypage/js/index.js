4771ff24cb9cdd2651853af8cfb5e5231c199dd4
9758hyoma <hyoma328hyoma@gmail.com>
2023-07-21 22:02:14 +0900

    20230721_授業前

--- mypage/static/mypage/js/index.js
+++ mypage/static/mypage/js/index.js
@@ -39,15 +39,55 @@ function clickbtn() {
 }
 
 function addPlaylist(id,name,artists){
-    const li = `<li class="user-music list-group-item bg-secondary text-white">
-                    <div class="user-music-update text-truncate">
-                            <p>${name}</p>
-                            <p>${artists}</p>
+    const li = `<li id="add" class="user-music list-group-item bg-secondary text-white">
+                    <div class="user-music-update d-flex justify-content-between">
+                        <div class="d-flex flex-column">
+                            <div class="text-truncate">Title : ${name}</div>
+                            <div class="text-truncate">Artists : ${artists}</div>
                             <input type="hidden" name="music_id[]" value="${id}">
+                        </div>
+                        <button class="btn btn-secondary btn-sm mt-2 ms-1 close-icon" value="削除" onClick="getElement();">×</button>
                     </div>
                 </li>`
+
+    // const newSpan = document.createElement('span');
+    //     newSpan.classList.add('close-icon');
+    //     newSpan.textContent = '✖';
+        
     document.getElementById("myplaylist").insertAdjacentHTML('beforeend',li);
+
+    // newSpan.addEventListener('click', () => {
+    //     newDiv.remove();
+    // });
+    // return newDiv;
 }
+
+function getElement(){
+    let dtn = document.getElementById('add');
+    dtn.remove();
+}
+// for (let j = 0; j < closeIcons.length; j++) {
+//     closeIcons[j].addEventListener('click', () => {
+//         items[j].remove();
+//     });
+// }
+
+//   // ボタンをクリックしたときの処理
+// btn.addEventListener('click', () => {
+//     form.appendChild(createNewForm());
+// });
+
+// document.querySelector('#deleteBtn1').addEventListener('click', () => {
+//     const element = document.getElementById('add');
+//     element.remove();
+// });
+// }
+
+
+// function clickBtn1(){
+//     var lia = document.getElementById("user-music-update");
+//     lia.remove();
+// }
 // 画像クリック
 // const thub = document.getElementById("thumb_album").value;
 // divElement.ondblclick = function() {
